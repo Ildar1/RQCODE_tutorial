7896113d56a2ad6b769487390a880c9b2a2fb508
Ildar Nigmatullin <ildarnn22@gmail.com>
2024-11-30 08:38:13 +0100

    Update PasswordUpdate.java

--- PasswordUpdate.java
+++ PasswordUpdate.java
@@ -7,27 +7,4 @@ import java.time.Period;
 
 public class PasswordUpdate extends Requirement {
 
-    private LocalDate lastPasswordUpdateDate;
-
-    public void PasswordUpdateEveryYear(LocalDate lastPasswordUpdateDate) {
-        this.lastPasswordUpdateDate = lastPasswordUpdateDate;
-    }
-
-    public PasswordUpdate(LocalDate lastPasswordUpdateDate) {
-        this.lastPasswordUpdateDate = lastPasswordUpdateDate;
-    }
-
-    @Override
-    public CheckStatus check() {
-        if (passwordLastUpdatedMoreThanOneYearAgo()) {
-            return CheckStatus.FAIL;
-        }
-        return CheckStatus.PASS;
-    }
-
-    private boolean passwordLastUpdatedMoreThanOneYearAgo() {
-        LocalDate currentDate = LocalDate.now();
-        Period period = Period.between(lastPasswordUpdateDate, currentDate);
-        return period.getYears() >= 1;
-    }
 }
