b3c48c4df2266488ef9022452f746db21f1d9b4d
JustSomeDude2001 <42960562+JustSomeDude2001@users.noreply.github.com>
2025-02-11 13:06:55 +0300

    Update lockout requirement file to fit the format in the project

--- PasswordLockoutRequirement.java
+++ PasswordLockoutRequirement.java
@@ -1,25 +1,44 @@
 public class PasswordLockoutRequirement extends Requirement {
-
-    private int failedAttempts;
-
-    @Override
-    public Checkable.CheckStatus check() {
-        if (failedAttempts >= 5) {
-            return CheckStatus.PASS;
-        } else {
-            return CheckStatus.FAIL;
-        }
-    }
+    private static final int MAX_FAILED_ATTEMPTS = 5;
+    private static final int STARTING_FAILED_ATTEMPTS = 0;
+    private Integer failedAttempts;
+    private String username;
     
     public PasswordLockoutRequirement(String username) {
-        failedAttempts = 0;
+        super("Requirement 2: Failed password attempts lockout Rquirement");
+        this.failedAttempts = STARTING_FAILED_ATTEMPTS;
+        this.username = username;
     }
 
+    // Increment amount of failed attempts
     public void recordFailedAttempt() {
         failedAttempts += 1;
     }
 
+    // Reset amount of failed attempts to zero
     public void resetFailedAttempts() {
-        failedAttempts = 0;
+        failedAttempts = STARTING_FAILED_ATTEMPTS;
+    }
+
+    // Check if the max amount of failed attempts reached
+    private boolean isMaxFailedAttemptsReached() {
+        return failedAttempts >= MAX_FAILED_ATTEMPTS;
+    }
+
+    @Override
+    public Checkable.CheckStatus check() {
+        if (username == null || failedAttempts == null) {
+            return CheckStatus.INCOMPLETE; // return INCOMPLETE if the requirement was not initialized
+        }
+
+        boolean attemptCountCheck = isMaxFailedAttemptsReached();
+
+        System.out.println("2: Amount of failed attempts exceeds " + MAX_FAILED_ATTEMPTS + " - " + (attemptCountCheck ? "PASS" : "FAIL"));
+
+        if (attemptCountCheck) {
+            return CheckStatus.PASS; // Return PASS if the amount of failed attempts exceeds the threshold for pass
+        } else {
+            return CheckStatus.FAIL; // Return FAIL if the lockout requirement failed
+        }
     }
 }
