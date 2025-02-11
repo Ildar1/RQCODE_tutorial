0fe1072985666c58cb624e1e9a52c016348d4cdd
JustSomeDude2001 <42960562+JustSomeDude2001@users.noreply.github.com>
2025-02-11 12:16:38 +0300

    Fix compilation errors, declare all required methods and classes

---
+++ PasswordLockoutRequirement.java
@@ -0,0 +1,23 @@
+public class PasswordLockoutRequirement extends Requirement {
+
+    @Override
+    public Checkable.CheckStatus check() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'check'");
+    }
+    
+    public PasswordLockoutRequirement(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'PasswordLockoutRequirement'");
+    }
+
+    public void recordFailedAttempt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recordFailedAttempt'");
+    }
+
+    public void resetFailedAttempts() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resetFailedAttempts'");
+    }
+}
